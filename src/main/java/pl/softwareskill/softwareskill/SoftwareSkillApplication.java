962d3b5395fffa1a00cc6d188d6a5bb60138dcd7
Artur Kowalczyk <artur.kowalczyk96@gmail.com>
2021-07-24 09:12:25 +0200

    1.0 Spring Boot - new project

---
+++ src/main/java/pl/softwareskill/softwareskill/SoftwareSkillApplication.java
@@ -0,0 +1,13 @@
+package pl.softwareskill.softwareskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareSkillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/softwareskill/softwareskill/SoftwareSkillApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.softwareskill.softwareskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareSkillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
